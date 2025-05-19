74e09d5b84ac2a0450bccfefe97b2ab2591bf0b6
Marta Tilman <marta.tilman@gmail.com>
2025-05-19 22:38:30 +0200

    promjene

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+import socket,os
+from app.database import engine, Base
+from app.models import book, user
 app = FastAPI()
 
 @app.get("/")
+def read_root():
+    hostname = socket.gethostname()
+    port = os.getenv("PORT", "unknown")
+    return {"message": f"Pozdrav s instance: {hostname}, port: {port}"}
+@app.get("/")
 def root():
-    return {"message": "Dobrodošla u eKnjižnicu!"}
+    return {"message": "eKnjižnica backend radi"}
