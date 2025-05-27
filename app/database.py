4d3a6e6a0cca1287e747e4079ec584f56de3b9ff
Marta Tilman <marta.tilman@gmail.com>
2025-05-27 21:34:51 +0200

    dodavanje dockera

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:marta@localhost:5432/eknjiznica"
+DATABASE_URL = "postgresql://postgres:marta@host.docker.internal:5432/eknjiznica"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
